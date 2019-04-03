1e2c82ca422c76a29646959c7c827d94ec93ec02
Period Four <periodfour@iMac-15.local>
2019-04-03 13:35:20 -0400

    Initial Commit

---
+++ Yang_Ximing_Reminder_App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Yang_Ximing_Reminder_App
+//
+//  Created by Period Four on 2019-04-03.
+//  Copyright © 2019 Period Four. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Yang_Ximing_Reminder_AppTests/Yang_Ximing_Reminder_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Yang_Ximing_Reminder_AppTests.swift
+//  Yang_Ximing_Reminder_AppTests
+//
+//  Created by Period Four on 2019-04-03.
+//  Copyright © 2019 Period Four. All rights reserved.
+//
+
+import XCTest
+@testable import Yang_Ximing_Reminder_App
+
+class Yang_Ximing_Reminder_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
